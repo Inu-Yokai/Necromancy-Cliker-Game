dfc8114a4e7a57f9c6bd2cc0c156fd87f08e5ee6
Inu-Yokai <168310777+Inu-Yokai@users.noreply.github.com>
2024-04-27 17:40:32 -0500

    Start
    Inu_Yokai's starting code.

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func new_game():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func quit():
+	get_tree().quit()
