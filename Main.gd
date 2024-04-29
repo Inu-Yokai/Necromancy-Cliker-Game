99c2cb40571eb80d446134c69f4674a11a46d1ac
Jonathan Melchor <melchorj@zohomail.com>
2024-04-29 18:46:43 -0500

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -1,20 +1,20 @@
 extends Node
 
-var soul_shards:int = 0
-var bug_count:int = 0
-var mouse_count:int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
+var soul_shards : int = 0
+var bug_count : int = 0
+var mouse_count : int = 0
+
+
 func _process(delta):
 	access_summons()
 	quit_to_title()
-	
+
+
 func _on_button_pressed():
 	soul_shards += 1
 	%Soul_Shards.text = "Soul Shards: " + str(soul_shards)
+
 
 func access_summons():
 	if soul_shards >= 10:
@@ -25,18 +25,19 @@ func access_summons():
 		$Necromancer_Area/BoxContainer2/Mouse.disabled = false
 
 
-
 func buy_bug():
 	if soul_shards >= 10:
 		soul_shards -= 10
 		%Soul_Shards.text = "Soul Shards: " + str(soul_shards)
 		bug_count += 1
 		$Necromancer_Area/BoxContainer/Bug/bug_count.text = str(bug_count)
-	
+
+
 func bug_shards():
 	if bug_count >= 5:
 		soul_shards += 1
 		%Soul_Shards.text = "Soul Shards: " + str(soul_shards)
+
 
 func buy_mouse():
 	if soul_shards >= 50:
@@ -44,17 +45,16 @@ func buy_mouse():
 		%Soul_Shards.text = "Soul Shards: " + str(soul_shards)
 		mouse_count += 1
 		$Necromancer_Area/BoxContainer2/Mouse/mouse_count.text = str(mouse_count)
-	
+
+
 func mouse_shards():
 	if mouse_count >= 10:
 		soul_shards += 2
 		%Soul_Shards.text = "Soul Shards: " + str(soul_shards)
-		
+
+
+#Allows user to quit at any time during game
 func quit_to_title():
 	if Input.is_action_just_pressed("quit"):
 		get_tree().change_scene_to_file("res://main_menu.tscn")
-		
-		
-		
-		
-		
+
