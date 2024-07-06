557eae0446aab8526dd1f81efafdca3fc9bdb1cc
Jonathan Melchor <melchorj@zohomail.com>
2024-07-06 02:33:21 -0500

    Add Rat

---
+++ Scripts/Undead.gd
@@ -0,0 +1,9 @@
+class_name UNDEAD_MOBS
+
+
+var id: String
+var name: String
+var strength: float
+var summon_time: float
+var number: int
+var summon_level: int
---
+++ rat.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+var id = "RT"
+var mob_name = "Rat"
+var strength = .1
+var summon_time = 3.0
+var number = 0
+var summon_level = 0
