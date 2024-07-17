7ba003b1baed14d4b6fb82da3b8efb76193563aa
Jonathan Melchor <melchorj@zohomail.com>
2024-07-17 00:42:52 -0500

    Updated with comments and remove excess code

--- Scripts/gravesite.gd
+++ Scripts/gravesite.gd
@@ -4,18 +4,8 @@ extends Area2D
 signal sf_trigger
 
 
-var sf_available = false
-
-
-func _on_mouse_entered():
-	sf_available = true
-
-
-func _on_mouse_exited():
-	sf_available = false
-
-
+# If left mouse is clicked and inside Area2d then triggers signal.
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			emit_signal("sf_trigger")
+			emit_signal("sf_trigger") # Connected to Game node | func _on_gravesite_sf_trigger
