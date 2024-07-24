5305ece34b1eb4d6c2888cd7569a261594142bf8
Jonathan Melchor <melchorj@zohomail.com>
2024-07-24 00:42:45 -0500

    Created battle script

---
+++ Scripts/Battle/battle.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+signal update_battle
+
+
+var player_army := 1.0 # Temp link to army strength
+var enemy_army := 1.0 # Temp link to town strength
+var player_roll : float
+var enemy_roll : float
+
+
+func battle(player_army, enemy_army):
+	# Minimum threshold first
+	var player_floor = min_thresh(player_army)
+	var enemy_floor = min_thresh(enemy_army)
+	# Continue until either army is at or below 0
+	while player_army >= 0 or enemy_army >= 0:
+		player_roll = randf_range(player_floor, player_army)
+		enemy_roll = randf_range(enemy_floor, enemy_floor)
+		if player_roll > enemy_roll:
+			player_army -= enemy_army
+		elif player_roll < enemy_roll:
+			enemy_army -= player_army
+		else:
+			player_army -= player_floor
+			enemy_army -= enemy_floor
+		# Update Bars
+
+
+func min_thresh(army):
+	# Creates a minimum amount to modify roll range and set least damage amount.
+	var floor = army * .1
+	return floor
